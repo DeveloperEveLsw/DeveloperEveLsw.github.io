5a8498499d34046d1700e9feb6b43aaf8e482b09
lws19121 <lws19121@gmail.com>
2025-06-07 12:52:14 +0900

    데이터 처리 끝

--- server.js
+++ server.js
@@ -26,13 +26,19 @@ app.get("/api", async (req, res) => {
         // 여기서 람다식의 e 의 경우 Response 객체이며 fetch의 반환값입니다
         const PlayerInfo = await fetch(
             `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamId}`
-        ).then( (e)=>e.json() );
-
+        ).then( body=> {
+            if (body.status == 429) { console.log(body); throw new Error("Too Many Requests") }
+            return body.json()
+        });
+        console.log(PlayerInfo)
         // 사용자의 게임 목록(소유중인)을 가져옵니다 그외 위와 같습니다
         const OwnedGames = await fetch(
             `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${steamId}&include_appinfo=true&include_played_free_games=true`
-        ).then( (e)=>e.json() );
-
+        ).then( (body)=> {
+            if (body.status == 429) { console.log(body); throw new Error("Too Many Requests") }
+            return body.json()
+        });
+        console.log(OwnedGames)
         // 위의 응답 내용중 games(Array 타입)의 map 메서드를 통해서 appid만 빼내서 다른 배열로 저장
         let appids = OwnedGames.response.games.map(game => game.appid);
 
@@ -49,19 +55,29 @@ app.get("/api", async (req, res) => {
             // 게임 목록을 가지고 map 메서드를 각각의 appid를 기반으로 게임 정보를 요청을 보냄
             appids.map(async appid => {
                 const res = await fetch(`https://store.steampowered.com/api/appdetails?appids=${appid}&filters=price_overview,release_date  `
-                ).then( body => body.json())
+                ).then( body => {
+                if (body.status == 429) { console.log(body.headers); throw new Error("Too Many Requests") }
+                return body.json()
+                })
                 return res[appid].success ? res : null
             })
-        )).filter(item => item !== null );
+        )).filter(item => item !== null
+        ).reduce((acc, obj) => {
+            return { ...acc, ...obj };
+        }, {});
 
-
+        
+        console.log(games)
         // 각 게임들의 도전과제 목록을 가져오는 api
         // 위와 거의 똑같지만 다른점은 응답값에 게임을 구분할 정보가 담겨져 있지 않음
         // 그렇기에 새로운 Object를 만들어서 appid를 키값으로 응답의 바디를 value로 넣음
         const achievements = (await Promise.all(
             appids.map(async appid=> {
                 const res = await fetch(`https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=${apiKey}&appid=${appid}&l=koreana`                   
-                ).then( body => body.json())
+                ).then( body => {
+                if (body.status == 429) { throw new Error("Too Many Requests") }
+                return body.json()
+                })
                 return res.game && res.game.gameVersion ? {[appid]: res} : null
             })
         )).filter(item => item !== null
@@ -69,14 +85,15 @@ app.get("/api", async (req, res) => {
             return { ...acc, ...obj };
         }, {});
 
-
-
         // 각 게임별 사용자의 도전과제 정보(클리어 정보, 클리어 시간 등등)
         // 이 경우도 appid를 추가하여줌 (새로운 Object를 만들지 않고 동적으로 키 추가)
         const playerAchievements = (await Promise.all(
             appids.map(async appid=>{
                 const res = await fetch(`https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/?key=${apiKey}&steamid=${steamId}&appid=${appid}`                    
-                ).then(body => body.json())
+                ).then(body => {
+                if (body.status == 429) { console.log(body.headers);  throw new Error("Too Many Requests") }
+                return body.json()
+                })
                 return res.playerstats.success ? {[appid]: res} : null
             })
         )).filter(item => item !== null
@@ -84,9 +101,19 @@ app.get("/api", async (req, res) => {
             return { ...acc, ...obj };
         }, {});
 
+        console.log(playerAchievements)
+
+        const new_OwnedGames = {
+            ...OwnedGames.response,
+            games: OwnedGames.response.games.map(item=>{
+                console.log(games[item.appid])
+                return {...item, price: (games[item.appid] && games[item.appid].data.price_overview) ? games[item.appid].data.price_overview.final : 0 }
+            })
+        }
+
         res_body = {
             PlayerInfo: PlayerInfo.response.players[0],
-            OwnedGames: OwnedGames.response,
+            OwnedGames: new_OwnedGames,
             games: games,
             achievements: achievements,
             playerAchievements: playerAchievements
@@ -94,7 +121,9 @@ app.get("/api", async (req, res) => {
         res.send(res_body)
 
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        console.log(error.message)
+        if (error.message == "Too Many Requests") { res.status(429).json({ error: error.message }); }
+        else {res.status(500).json({ error: error.message });}
     }
 });
 
